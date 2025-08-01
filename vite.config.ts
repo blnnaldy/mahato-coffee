38d2a177edf0dd8eb2f2e7f4d303bdcfe452f063
blnaldy <blnaldy4@gmail.com>
2025-08-02 02:19:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ✅ tambahkan ini untuk Netlify atau deploy static
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
